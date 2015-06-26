0fd3fba15c4eb786ab76d1b3c5e77fb6c8bc6b68
lilu <lilugirl2005@126.com>
2015-06-27 00:10:37 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+$link=array();
+$link["jianshu"]=array("http://www.jianshu.com","简书","h1","h2","p2",".article");
+$link["cnblogs"]=array("http://www.cnblogs.com/","博客园","h1","","","");
+define("PASSWORD", "test");
+?>
